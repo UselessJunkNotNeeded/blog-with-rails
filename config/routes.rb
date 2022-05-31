c13f2e8232a471eb1370bac44edc240872f3af58
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-31 20:23:02 +0530

    Fixed linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "/users/:user_id/posts" to: "posts#index"
-  get "/users/:user_id/posts/:id" to: "posts#show"
-  get "/users" to: "users#index"
-  get "/users/:id/posts/:id" to "users#show"
+  root "pages#home"
+  get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/posts/:id", to: "posts#show"
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
 end
