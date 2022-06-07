c17b7724ea23f2521e7d07329b15d50292e115f0
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-07 19:24:35 +0530

    Added show user page with three recent posts if available

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "users#index"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
+  # get "/users/:user_id/posts", to: "posts#index"
+  # get "/users/:user_id/posts/:id", to: "posts#show"
+  # get "/users", to: "users#index"
+  # get "/users/:id", to: "users#show"
+  resources :users do
+    resources :posts
+  end
 end
