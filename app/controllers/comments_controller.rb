8ccd174b62e713db8e22436f27b31477487a45b6
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-08 23:04:21 +0530

    Added functionality to create comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = params[:user_id]
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      flash[:notice] = "Comment created successfully"
+      redirect_to user_posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author: params[:user_id])
@@ -8,5 +9,24 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(user_params)
+    @post.author_id = params[:user_id]
+    if @post.save
+      flash[:notice] = "Post created successfully"
+      redirect_to user_posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:post).permit(:title, :text)
   end
 end
