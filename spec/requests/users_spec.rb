90cf285740b6dac33b1492caa7a8b16a958f22ab
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-09 17:31:10 +0530

    Added tests for user model

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe 'Users', type: :request do
       it 'shows correct view' do
         expect(response).to render_template(:index)
       end
-
-      it 'Check for the response body' do
-        expect(response.body).to include('All the users will be displayed here')
-      end
     end
   end
 
@@ -23,10 +19,6 @@ RSpec.describe 'Users', type: :request do
       get 'users/1'
       it 'Check for response code as 200' do
         expect(response).to have_http_status(:ok)
-      end
-
-      it 'check for the response body' do
-        expect(response.body).to include('A particular user will be displayed here')
       end
 
       it 'shows correct view' do
