c239c5a65287bbea70ad5b518e0c1dfb5b430512
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-09 12:09:19 +0530

    Added like button functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,23 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.new()
+    @like.author_id = params[:user_id]
+    @like.post_id = params[:post_id]
+    if @like.save
+      flash[:notice] = "Comment created successfully"
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
