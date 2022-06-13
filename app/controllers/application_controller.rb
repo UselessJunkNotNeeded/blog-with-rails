5f7074229ae9ec6156adf2cc3282ba5c330bf1cd
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-13 20:17:38 +0530

    Added devise methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
+  protect_from_forgery prepend: true
+  before_action :permitted_parameters, if: :devise_controller?
+
+  protected
+  def permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio])
   end
 end
